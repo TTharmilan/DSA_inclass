82bece1e54943e042a37311cd3d426b366dce3e2
TTharmilan <129476295+TTharmilan@users.noreply.github.com>
2023-05-23 17:39:50 +0530

    in-class lab11(MST).cpp
    code for prim's algorithm

--- in-class lab11(MST).cpp
+++ in-class lab11(MST).cpp
@@ -1,77 +1,47 @@
-#include <iostream>
-#include <climits>
-
-#define V 6
+#include <bits/stdc++.h>
 using namespace std;
-
-int minKey(int key[], bool mstSet[])
-{
-    int min = INT_MAX, min_index;
-
-    for (int j = 0; j < V; j++)
-    {
-        if (mstSet[j] == false && key[j] < min)
-        {
-            min = key[j];
-            min_index = j;
-        }
-    }
-
-    return min_index;
+#define V 6
+bool createMST(int u, int v, vector<bool> inMST){
+    if (u == v)
+        return false;
+    if (inMST[u] == false && inMST[v] == false)
+        return false;
+    else if (inMST[u] == true && inMST[v] == true)
+        return false;
+        return true;
 }
-
-void printMST(int parent[], int graph[V][V])
-{
-    for (int i = 1; i < V; i++)
-    {
-        cout << parent[i] << " -- " << i <<"    "<<graph[i][parent[i]] << endl;
-    }
-}
-
-void prim(int graph[V][V])
-{
-    int parent[V];
-    int key[V];
-    bool mstSet[V];
-
-    for (int k = 0; k < V; k++)
-    {
-        key[k] = INT_MAX;
-        mstSet[k] = false;
-    }
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int c = 0; c < V - 1; c++)
-    {
-        int u = minKey(key, mstSet);
-
-        mstSet[u] = true;
-
-        for (int v = 0; v < V; v++)
-        {
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-            {
-                parent[v] = u;
-                key[v] = graph[u][v];
+void printMST(int adj[][V]){
+    vector<bool> inMST(V, false);
+    inMST[0] = true;
+    int c = 0;
+    while (c < V - 1) {
+        int min = INT_MAX, a = -1, b = -1;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (adj[i][j] < min) {
+                    if (createMST(i, j, inMST)) {
+                        min = adj[i][j];
+                        a = i;
+                        b = j;
+                    }
+                }
             }
         }
+        if (a != -1 && b != -1) {
+            cout<<a<<" -- "<<b<<"    "<<min<<endl;
+            c++;
+            inMST[b] = inMST[a] = true;
+        }
     }
-
-    printMST(parent, graph);
 }
-
-int main()
-{
-    int graph[V][V] = {{0, 3, 0, 0, 0, 1},
-                       {3, 0, 2, 1, 10, 0},
-                       {0, 2, 0, 3, 0, 5},
-                       {0, 1, 3, 0, 5, 0},
-                       {0, 10, 0, 5, 0, 4},
-                       {1, 0, 5, 0, 4, 0}};
-
-    prim(graph);
-
+int main() {
+    int adj[][V] =
+    { { INT_MAX, 3, INT_MAX, INT_MAX, INT_MAX , 1 },
+    { 3, INT_MAX, 2, 1, 10, INT_MAX },
+    { INT_MAX, 2, INT_MAX,3, INT_MAX, 5 },
+    {   INT_MAX,1,3,  INT_MAX, 5, INT_MAX },
+    {  INT_MAX,10, INT_MAX, 5, INT_MAX, 4 },
+    { 1,  INT_MAX,5,  INT_MAX, 4, INT_MAX }, };
+    printMST(adj);
     return 0;
 }
